caf74b75d8dd1b21febbbbc790b5df9b862dea24
alexi <alexi@DESKTOP-OIL3S44>
2022-01-22 02:27:52 -0300

    Add project files.

---
+++ EnvyNoCarkill/Commands/CommandCarkill.cs
@@ -0,0 +1,41 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvyNoCarkill.Commands
+{
+    internal class CommandCarkill : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+
+        public string Name => "carkill";
+
+        public string Help => "plugin by margarita";
+
+        public string Syntax => String.Empty;
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            var instance = EnvyNoCarkillPlugin.Instance;
+
+            if (instance.CarkillEnabled)
+            {
+                instance.CarkillEnabled = false;
+                UnturnedChat.Say(caller, "Desactivaste el daño al atropellar");
+            }
+            else
+            {
+                instance.CarkillEnabled = true;
+                UnturnedChat.Say(caller, "Desactivaste el daño al atropellar");
+            }
+        }
+    }
+}
---
+++ EnvyNoCarkill/EnvyNoCarkillPlugin.cs
@@ -0,0 +1,37 @@
+﻿using Rocket.Core.Plugins;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace EnvyNoCarkill
+{
+    public class EnvyNoCarkillPlugin : RocketPlugin
+    {
+        public static EnvyNoCarkillPlugin Instance { get; set; }
+        public bool CarkillEnabled { get; set; }
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.Log("Plugin By Margarita#8172");
+            DamageTool.damagePlayerRequested += DamageTool_damagePlayerRequested;
+            CarkillEnabled = false;
+        }
+
+        private void DamageTool_damagePlayerRequested(ref DamagePlayerParameters parameters, ref bool shouldAllow)
+        {
+            if(parameters.cause == EDeathCause.VEHICLE & !CarkillEnabled)
+            {
+                shouldAllow = false;
+            }
+        }
+
+        protected override void Unload()
+        {
+            DamageTool.damagePlayerRequested -= DamageTool_damagePlayerRequested;
+        }
+    }
+}
